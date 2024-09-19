75ceeb48c937e17522811685ee17616aaab13002
Roshni Kumari <144609293+Roshni009@users.noreply.github.com>
2024-09-20 00:56:26 +0530

    Update

--- src/components/job-card.jsx
+++ src/components/job-card.jsx
@@ -5,7 +5,8 @@ import { Heart, MapPinIcon, Trash2Icon } from 'lucide-react';
 import { Button } from './ui/button';
 import { Link } from 'react-router-dom';
 import useFetch from '@/hooks/use-fetch';
-import { saveJob } from '@/api/apiJobs';
+import { deleteJob, saveJob } from '@/api/apiJobs';
+import { BarLoader } from 'react-spinners';
 
 const JobCard = ({
      job,
@@ -31,18 +32,31 @@ const JobCard = ({
          onJobSaved();
     };
 
+    const { loading: loadingDeleteJob, fn: fnDeleteJob } = useFetch(deleteJob, {
+        job_id: job.id,
+    });
+
+    const handleDeleteJob = async () => {
+        await fnDeleteJob();
+        onJobSaved();
+    };
+
+
     useEffect(() => {
         if(savedJob !== undefined) setSaved(savedJob?.length > 0);
     }, [savedJob]);
 
   return (
   <Card className="mx-5 flex flex-col">
+    {loadingDeleteJob && (
+        <BarLoader className='mt-4' width={"100%"} color='#36d7b7'/>
+    )}
     <CardHeader>
         <CardTitle className="flex justify-between font-bold">
             {job.title}
 
-            {!isMyJob && (
-                 <Trash2Icon fill='red' size={18} className='text-red-300 cursor-pointer' />
+            {isMyJob && (
+                 <Trash2Icon fill='red' size={18} className='text-red-300 cursor-pointer' onClick={handleDeleteJob}/>
             )}
         </CardTitle>
     </CardHeader>
--- src/pages/my_job.jsx
+++ src/pages/my_job.jsx
@@ -1,9 +1,29 @@
+import CreatedApplications from '@/components/created-applications';
+import CreatedJobs from '@/components/created-jobs';
+import { useUser } from '@clerk/clerk-react';
 import React from 'react'
+import { BarLoader } from 'react-spinners';
 
 const MyJob = () => {
+
+  const { user, isLoaded } = useUser();
+
+  if(!isLoaded) {
+    return <BarLoader className='mb-4' width={"100%"} color='#36d7b7'/>
+  }
   return (
-    <div>my_job</div>
-  )
-}
+    <div>
+       <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+       {user?.unsafeMetadata?.role === "candidate" ? "My Applications" : "My Jobs"}
+      </h1>
+       {user?.unsafeMetadata?.role === "candidate" ? (<CreatedApplications />
+       ): (
+        <CreatedJobs />
+       )      
+      }
+
+    </div>
+  );
+};
 
 export default MyJob;
